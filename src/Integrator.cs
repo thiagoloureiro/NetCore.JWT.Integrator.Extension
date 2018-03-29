6b087f5758f139c4e7f158b50cfa906dedfad055
Thiago Loureiro de Azevedo <thiago.loureiro.de.azevedo@playngo.com>
2018-03-29 16:47:11 +0200

    Cleanup

--- src/Integrator.cs
+++ src/Integrator.cs
@@ -10,31 +10,14 @@ using Microsoft.VisualStudio.Shell.Interop;
 
 namespace JWTIntegrator
 {
-    /// <summary>
-    /// Command handler
-    /// </summary>
     internal sealed class Integrator
     {
-        /// <summary>
-        /// Command ID.
-        /// </summary>
         public const int CommandId = 0x0100;
 
-        /// <summary>
-        /// Command menu group (command set GUID).
-        /// </summary>
         public static readonly Guid CommandSet = new Guid("2298e3cc-ff51-4aac-afbb-2101d893dd63");
 
-        /// <summary>
-        /// VS Package that provides this command, not null.
-        /// </summary>
         private readonly Package package;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Integrator"/> class.
-        /// Adds our command handlers for menu (commands must exist in the command table file)
-        /// </summary>
-        /// <param name="package">Owner package, not null.</param>
         private Integrator(Package package)
         {
             if (package == null)
@@ -53,18 +36,12 @@ namespace JWTIntegrator
             }
         }
 
-        /// <summary>
-        /// Gets the instance of the command.
-        /// </summary>
         public static Integrator Instance
         {
             get;
             private set;
         }
 
-        /// <summary>
-        /// Gets the service provider from the owner package.
-        /// </summary>
         private IServiceProvider ServiceProvider
         {
             get
@@ -73,22 +50,11 @@ namespace JWTIntegrator
             }
         }
 
-        /// <summary>
-        /// Initializes the singleton instance of the command.
-        /// </summary>
-        /// <param name="package">Owner package, not null.</param>
         public static void Initialize(Package package)
         {
             Instance = new Integrator(package);
         }
 
-        /// <summary>
-        /// This function is the callback used to execute the command when the menu item is clicked.
-        /// See the constructor to see how the menu item is associated with this function using
-        /// OleMenuCommandService service and MenuCommand class.
-        /// </summary>
-        /// <param name="sender">Event sender.</param>
-        /// <param name="e">Event args.</param>
         private void MenuItemCallback(object sender, EventArgs e)
         {
             string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
